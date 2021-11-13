6ea71fce43c22f9d08e8783e88ab743ca45816b3
elliott fuller <efuller1101@gmail.com>
2021-11-13 16:18:32 +1100

    WIP

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,40 @@ class TasksController < ApplicationController
     def index 
         @tasks = Task.all
     end
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        @task.save
+        redirect_to task_path(@task)
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = task.find(params[:id])
+        @task.update(params[:task])
+        redirect_to task_path(@task)
+    end
+
+    def destroy
+        @task = task.find(params[:id])
+        @task.destroy
+        redirect_to task_path
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :details)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
-  get '/tasks/new' to: 'tasks#'
-end
+  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: "tasks#edit"
+  patch '/tasks/:id', to: "tasks#update"
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  end
